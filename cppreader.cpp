c3f9bad63b5d0bc3ffa5a5b6bd500f574b4f3e26
DJ MacHack <mail@hendrik-haas.de>
2017-06-23 13:58:51 +0200

    1st

---
+++ cppreader.cpp
@@ -0,0 +1,8 @@
+//
+// Created by DJ_MacHack on 23.06.2017.
+// Mtknr.: 374771
+// mail@hendrik-haas.de
+// Partner: Julian Bruna
+//
+
+#include "cppreader.h"
---
+++ cppreader.h
@@ -0,0 +1,17 @@
+//
+// Created by DJ_MacHack on 23.06.2017.
+// Mtknr.: 374771
+// mail@hendrik-haas.de
+// Partner: Julian Bruna
+//
+
+#ifndef PROG2_8_CPPREADER_H
+#define PROG2_8_CPPREADER_H
+
+
+class cppreader {
+
+};
+
+
+#endif //PROG2_8_CPPREADER_H
